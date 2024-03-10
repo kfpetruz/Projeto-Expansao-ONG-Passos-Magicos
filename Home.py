890a39b0541599c195a02973fa06603072cc6b0f
kfpetruz <keilamker@gmail.com>
2024-03-10 18:27:33 -0300

    comit inicial

---
+++ Home.py
@@ -0,0 +1,10 @@
+# Importação das bibliotecas
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# Configuração da página
+st.set_page_config(page_title= 'ONG Passos Mágicos', layout='wide', page_icon= '🤝')
+
+# Título da página
+st.title('Projeto ONG Passos Mágicos 🤝')
