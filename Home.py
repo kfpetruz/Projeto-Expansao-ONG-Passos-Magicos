2977864818fb0044bd73287487e9d85539ec42aa
kfpetruz <keilamker@gmail.com>
2024-03-17 20:35:43 -0300

    Gráfico e explanação do modelo

--- Home.py
+++ Home.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import matplotlib.pyplot as plt
 
 # Configuração da página
 st.set_page_config(page_title= 'ONG Passos Mágicos', layout='wide', page_icon= '🤝')
@@ -17,6 +18,7 @@ dados = [(2016, 70), (2017, 300), (2018, 550), (2019, 812), (2020, 841), (2021, 
 # Criar um DataFrame a partir da lista de tuplas
 linha_do_tempo_ong = pd.DataFrame(dados, columns=['Ano', 'Quantidade de alunos'])
 
+
 dados_estimados = pd.read_csv('Base de dados\\populacao_estimativa_2001_2021.csv',sep = ';', encoding='utf-8-sig')
 novos_nomes = dados_estimados.iloc[2]
 dados_estimados = dados_estimados.rename(columns=novos_nomes)
@@ -29,16 +31,47 @@ dados_estimados['Município'] = dados_estimados['Município'].str.replace(')', '
 dados_estimados['Município'] = dados_estimados['Município'].str.replace('SP', '')
 dados_estimados['Município'] = dados_estimados['Município'].str.strip()
 dados_estimados= dados_estimados[dados_estimados['Município']=='Embu-Guaçu']
+
+dados_estimados_ultimos_anos = dados_estimados[dados_estimados.columns[-6:-1]].T.reset_index()
+dados_estimados_ultimos_anos = dados_estimados_ultimos_anos.rename(columns= {'index': 'Ano', 168: 'População do município'})
+dados_estimados_ultimos_anos['Ano'] = dados_estimados_ultimos_anos['Ano'].astype('int64')
+df_alunos_populacao = pd.merge(linha_do_tempo_ong, dados_estimados_ultimos_anos, on='Ano', how='left')
+
+#GRÁFICO ALUNOS VS POPULAÇÃO
+# Criando uma figura e eixos com tamanho personalizado
+fig, ax1 = plt.subplots(figsize=(10, 6))
+# Plotando a primeira série de dados (Quantidade de alunos)
+ax1.plot(df_alunos_populacao['Ano'], df_alunos_populacao['Quantidade de alunos'], color='#0145AC', label='Quantidade de alunos', marker='o')
+ax1.set_xlabel('Ano')
+ax1.set_ylabel('Quantidade de alunos', color='#0145AC')
+# Adicionando rótulos de dados (annotações) para os pontos específicos
+for x, y in zip(df_alunos_populacao['Ano'], df_alunos_populacao['Quantidade de alunos']):
+    ax1.annotate(f'{y}', (x, y), textcoords="offset points", xytext=(0,10), ha='center', color='#0145AC')
+# Criando o segundo eixo y (População do município)
+ax2 = ax1.twinx()
+ax2.plot(df_alunos_populacao['Ano'], df_alunos_populacao['População do município'], color='#82C7A5', label='População do município', marker='o')
+ax2.set_ylabel('População do município', color='#82C7A5')
+# Adicionando rótulos de dados (annotações) para os pontos específicos
+for x, y in zip(df_alunos_populacao['Ano'], df_alunos_populacao['População do município']):
+    ax2.annotate(f'{y}', (x, y), textcoords="offset points", xytext=(0,10), ha='center', color='#82C7A5')
+# Adicionando legenda fora do gráfico
+lines, labels = ax1.get_legend_handles_labels()
+lines2, labels2 = ax2.get_legend_handles_labels()
+ax1.legend(lines + lines2, labels + labels2, loc='center left', bbox_to_anchor=(0.1, 0.1))
+# Exibindo o gráfico
+plt.title('Quantidade de Alunos vs. População do Município')
+
 dados_2022 = pd.read_excel('tb_populacao_economia_idade_distancia.xlsx')
 dados_2022 =  dados_2022[dados_2022['Município']=='Embu-Guaçu']
 dados_estimados['2022'] = dados_2022['População no último censo'].values
+
 
 
 ## VISUALIZAÇÃO NO STREAMLIT
 aba1, aba2, aba3 = st.tabs(['Sobre a ONG', 'Fatores de sucesso', 'Impacto Social'])
 with aba1:
     st.title('Sobre a ONG')
-    st.markdown('<p style="text-align: justify;"> A Passos Mágicos é uma associação sem fins lucrativos de educação complementar,não formal e assistência social que possui o objetivo de transformar a vida de crianças e adolescentes em situação de vulnerabilidade social no município de Embu-Guaçu. A Associação teve início em 1992, auxiliando crianças em orfanato com origem da idealização de seus fundadores: Michelle Flues Ivanoff, Dimitri Ivanoff, Carol Ivanoff e Alexandre Ivanoff.</p>', unsafe_allow_html = True)
+    st.markdown('<p style="text-align: justify;"> A Passos Mágicos é uma associação sem fins lucrativos de educação complementar, não formal e assistência social que possui o objetivo de transformar a vida de crianças e adolescentes em situação de vulnerabilidade social no município de Embu-Guaçu. A Associação teve início em 1992, auxiliando crianças em orfanato com origem da idealização de seus fundadores: Michelle Flues Ivanoff, Dimitri Ivanoff, Carol Ivanoff e Alexandre Ivanoff.</p>', unsafe_allow_html = True)
     
     st.subheader('O que fazemos?')
     st.markdown('<p style="text-align: justify;">A Associação Passos Mágicos atua dentro do Município de Embu-Guaçu, zona sul de São Paulo, com um programa educacional para crianças e jovens, oferecendo um processo que visa três pontos de impacto: oferecer uma educação de qualidade, suporte psicológico e ampliar a visão de mundo de cada aluno impactado.</p>', unsafe_allow_html = True)
@@ -50,7 +83,13 @@ with aba1:
     with col3:
         st.markdown(f"<h2 style='{cor_estilizada}'>Mais de 10.500</h2> <span style='{fonte_negrito}'>horas de aula no Programa de Aceleração do Conhecimento (PAC) </span>", unsafe_allow_html=True)
 
+    st.markdown("<div style='height: 40px;'></div>", unsafe_allow_html=True) #Linha 
+    st.pyplot(fig)
+
+
 with aba2:
+
+
     col1, col2 = st.columns(2)
     with col1: 
         st.title('Fatores chave de sucesso')
@@ -66,7 +105,7 @@ with aba3:
 
 # 1ª aba - Análise Dados Históricos
 # 2ª aba - Resultado das ações na cidade - Fatores-Chave de Sucesso
-# 3ª aba - Desempenho dos alnos - Análise do Impacto Emocional e Social
+# 3ª aba - Desempenho dos alUnos - Análise do Impacto Emocional e Social
 # 4ª aba - Aprimoramento de estratégias e operações Futuras (PIX, Potencias cidades para expansão(Modelo) e Previsão de aumento de alunos (Quantidade de alunos para os próximos anos))
 # 5ª aba - Sobre
 
--- pages/Projeto de Expansão.py
+++ pages/Projeto de Expansão.py
@@ -9,12 +9,35 @@ st.set_page_config(page_title= 'ONG Passos Mágicos', layout='wide', page_icon= 
 # Título da página
 st.title('Projeto de expansão 🤝')
 
+dados_externos = pd.read_excel('tb_populacao_economia_idade_distancia.xlsx')
+
 ## VISUALIZAÇÃO NO STREAMLIT
 aba1, aba2= st.tabs(['Modelo', 'Propostas'])
 with aba1:
-    st.title('Modelo matemático')
+    st.markdown('<p style="text-align: justify;"> Para auxiliar a ONG em seu desejo de expandir as atividades, ampliando a capacidade para que ela possa atender e mudar a vida de centenas de crianças e adolescentes como vem fazendo há anos na cidade de Embu-Guaçu, foram criados dois modelos levando em consideração dados econômicos, de população, educacionais e de distância da cidade sede.</p>', unsafe_allow_html = True)
 
-    st.title('Modelo K-means')
+    st.markdown('<p style="text-align: justify;"> Em ambos os modelos foram listadas as 645 cidades do estado de São Paulo e atribuídos pesos aos seus indicadores econômicos, populacionais e educacionais por ordem de prioridade:.</p>', unsafe_allow_html = True)
+
+    st.markdown('<p style="text-align: justify;"> <strong> Quanto menor, maior o peso: </strong> </p>', unsafe_allow_html = True)
+    st.markdown('- <p style="text-align: justify;"> Salário médio dos trabalhadores formais;</p>', unsafe_allow_html = True)
+    st.markdown('- <p style="text-align: justify;"> Índice de desenvolvimento humano municipal (IDHM);</p>', unsafe_allow_html = True)
+    st.markdown('- <p style="text-align: justify;"> PIB per capita;</p>', unsafe_allow_html = True)
+    st.markdown('- <p style="text-align: justify;"> Percentual de alunos matriculados;</p>', unsafe_allow_html = True)
+
+    st.markdown('<p style="text-align: justify;"> <strong> Quanto maior, maior o peso: </strong> </p>', unsafe_allow_html = True)
+    st.markdown('- <p style="text-align: justify;"> Densidade demográfica - habitantes/km²;</p>', unsafe_allow_html = True)
+    st.markdown('- <p style="text-align: justify;"> População no último censo; </p>', unsafe_allow_html = True)
+    st.markdown('- <p style="text-align: justify;"> Percentual de crianças e jovens em idade elegível;</p>', unsafe_allow_html = True)
+
+
+    st.subheader('Modelo matemático')
+    st.markdown('##### Dataset')
+
+    st.dataframe(dados_externos.head())
+
+
+
+    st.subheader('Modelo K-means')
 
 with aba2:
     st.title('Propostas para Expansão')
